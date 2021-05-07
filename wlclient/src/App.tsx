3584b106b74fd1e47b0c4289b6974d695ca23409
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-08 00:30:17 +0800

    Basic implementation of client-side sockets

--- wlclient/src/App.tsx
+++ wlclient/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import io from 'socket.io-client';
+
+const ENDPOINT = process.env.REACT_APP_ENDPOINT || 'http://localhost:8000';
 
 function App(): React.ReactElement {
+
+    React.useEffect(() => {
+        const socket = io(ENDPOINT);
+        console.log('socket');
+        return () => {
+            socket.disconnect();
+        }
+    }, []);
+
     return (
         <div className="App">
-            <Header chapter={1} deadline={new Date('2021-05-07T23:59:00')} />
+            <Header chapter={1} deadline={new Date('2021-05-08T01:59:59')} />
             <div className="App-container">Test2</div>
         </div>
     );
