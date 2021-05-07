a6d5cca9d90cc9417090047df8952f0809fa0a96
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-08 00:28:29 +0800

    Move logs to logs/

--- wlserver/src/logger.ts
+++ wlserver/src/logger.ts
@@ -3,8 +3,8 @@ import * as Transport from 'winston-transport';
 import expressWinston from 'express-winston';
 
 const transports: Transport[] = [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' })
 ]
 
 if (process.env.NODE_ENV !== 'production') {
