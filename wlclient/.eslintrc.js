3bd781b5a8b1f4b180d278347ef16e897944d6fb
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-07 20:54:37 +0800

    Format wlclient stuff

--- wlclient/.eslintrc.js
+++ wlclient/.eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescripteslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-  ],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    env: {
+        node: true,
+        browser: true,
+    },
 };
