6e8c989b524e7dd87bdc6ea0a0b7f56faa57acd5
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-07 22:01:07 +0800

    Setup basic timer for client

--- wlclient/src/App.tsx
+++ wlclient/src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
+function App(): React.ReactElement {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Header chapter={1} deadline={new Date('2021-05-07T23:59:00')} />
+            <div className="App-container">Test2</div>
         </div>
     );
 }
