6e51883b7f3e9da14e59378fe78eae3534e7898e
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-07 23:53:15 +0800

    Basic setup for SocketIO (server-side)

--- wlserver/index.ts
+++ wlserver/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
-import { expressLogger } from './src/logger';
+import logger, { expressLogger } from './src/logger';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(expressLogger);
 
 app.get('/', (_, res) => res.send('Express + TypeScript Server'));
+
+io.on('connection', (socket: Socket<DefaultEventsMap, DefaultEventsMap>): void => {
+    logger.log('info', 'New client connected');
+    socket.on('disconnect', (): void => {
+        logger.log('info', 'Client disconnected');
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`[server]: Server is running at https://localhost:${PORT}`)
