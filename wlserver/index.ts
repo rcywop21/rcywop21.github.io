a9681f03578e24f7bae294704f34dad20a81e6db
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-13 01:11:35 +0800

    Implement basic tracking of connections

--- wlserver/index.ts
+++ wlserver/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import logger, { expressLogger } from './src/logger';
 import http from 'http';
 import { Server } from 'socket.io';
+import auth, { asValidClientType } from './src/auth';
+import { deregisterToken, registerToken } from './src/connections';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -21,9 +23,27 @@ app.get('/', (_, res) => res.send('This is a backend server.'));
 
 io.on('connection', (socket) => {
     logger.log('info', 'New client connected');
-    socket.on('disconnect', () => {
-        logger.log('info', 'Client disconnected');
-    });
+
+    socket.on('authenticate', async (payload) => {
+        const { id, mode, pass } = payload;
+
+        try {
+            if (typeof id === 'number' && typeof pass === 'string' && asValidClientType(mode)) {
+                const token = await auth(mode, id, pass);
+                registerToken(token);
+                socket.emit('auth_ok', token);
+                logger.log('info', `Auth success: mode ${mode} id ${id} token ${token.payload} expiring ${new Date(token.validUntil).toISOString()}`);
+                socket.on('disconnect', () => {
+                    deregisterToken(token);
+                    logger.log('info', `Authenticated user disconnected. Mode ${mode} id ${id} token ${token.payload}`)
+                });
+            } else {
+                throw 'malformed_request';
+            }
+        } catch(e) {
+            socket.emit('error', e);
+        }
+    })
 });
 
 server.listen(PORT, () => {
---
+++ wlserver/src/connections.ts
@@ -0,0 +1,25 @@
+import { AuthToken } from "./auth";
+
+const connections: Record<string, AuthToken> = {};
+
+export function registerToken(token: AuthToken): boolean {
+    if (token.validUntil <= Date.now()) return false;
+    connections[token.payload] = token;
+}
+
+export function validateToken(token: AuthToken): boolean {
+    const stored = connections[token.payload];
+    return stored?.id === token.id && stored.validUntil > Date.now();
+}
+
+export function deregisterToken(token: AuthToken): void {
+    delete connections[token.id];
+}
+
+export function cleanup(): void {
+    Object.entries(connections).forEach(([key, value]) => {
+        if (value.validUntil <= Date.now()) {
+            delete connections[key];
+        }
+    })
+}
