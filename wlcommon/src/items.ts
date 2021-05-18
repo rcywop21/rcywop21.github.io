877edab321d40020f9f0283c17996d58966a712c
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-18 23:33:39 +0800

    Add a few items to wlcommon

--- wlcommon/src/items.ts
+++ wlcommon/src/items.ts
@@ -10,3 +10,26 @@ export interface ItemRecord {
     item: Item;
     qty: number;
 }
+
+const itemList: Item[] = [
+    {
+        id: 'Map',
+        name: 'Map of the Undersea',
+        description: 'It shows you the way to most of the major locations in the Undersea.',
+    },
+    {
+        id: 'OxygenGuide',
+        name: 'Guide to Oxygen Streams',
+        description: 'It describes most of the Oxygen Streams in the Undersea as well as how to get Oxygen from them.',
+    },
+    {
+        id: 'MermaidDoll',
+        name: 'Mermaid Doll',
+        description: "It's a doll of The Little Mermaid! How cute.",
+    },
+]
+
+export const items: Record<ItemId, Item> = {};
+itemList.forEach((item) => {
+    items[item.id] = item;
+});
