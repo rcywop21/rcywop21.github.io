b54386ee9f5251e8c7ee2cfd4a7186306829d9eb
zoeykobe <kelvinzhou981209@gmail.com>
2021-05-20 22:38:13 +0800

    Update Game State Function
    Add update game state function

--- wlclient/src/components/Game.tsx
+++ wlclient/src/components/Game.tsx
@@ -3,7 +3,9 @@ import TopBar from './TopBar/TopBar';
 import Location from './Location/Location';
 import BottomBar from './BottomBar/BottomBar';
 import Journal from './Journal/Journal';
+import { GlobalState, PlayerState } from 'wlcommon';
 import './Game.css';
+import { socket } from '../socket/socket';
 
 export interface GameProps {
     groupId: string;
@@ -27,12 +29,20 @@ export interface GameProps {
         Popup?
             - Various popup windows
 */
-    
 
 const Game = (props: GameProps): React.ReactElement => {
     const { groupId } = props;
+    const [ playerState, setPlayerState ] = React.useState(undefined);
+    const [ globalState, setGlobalState ] = React.useState(undefined);
     
     //gamestate processing and listening
+    React.useEffect(() => {
+        socket.on('player_update', (newGameState: React.SetStateAction<undefined>) => setPlayerState(newGameState) );
+    })
+
+    React.useEffect(() => {
+        socket.on('global_update', (newGameState: React.SetStateAction<undefined>) => setGlobalState(newGameState) )
+    })
     
     return (
         <div className="game">
