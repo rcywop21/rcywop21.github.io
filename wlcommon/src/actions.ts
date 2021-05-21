67c3c44877e18a840551e7d62047a07863156391
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-22 00:48:18 +0800

    Reorganize actions in wlcommon again

--- wlcommon/src/actions.ts
+++ wlcommon/src/actions.ts
@@ -3,27 +3,25 @@ import { locationIds, locationsMapping } from "./locations";
 
 export type Action = string;
 
-type ActionRecord = Record<string, Action>;
-
-export const ALL_UNDERWATER: ActionRecord = {
+export const ALL_UNDERWATER = {
     STORE_OXYGEN: 'store_oxygen',
     WITHDRAW_OXYGEN: 'withdraw_oxygen',
     RESURFACE: 'resurface',
 };
 
-export const ALL_OXYGEN: ActionRecord = {
+export const ALL_OXYGEN = {
     GET_OXYGEN: 'get_oxygen',
 };
 
-export const specificActions: Record<LocationId, Record<Action, string>> = {
-    [locationIds.SHORES]: {
+export const specificActions = {
+    SHORES: {
         DIVE: 'dive',
     },
-    [locationIds.CORALS]: {
+    CORALS: {
         EXPLORE: 'explore',
         LEARN_LANG: 'learn_lang',
     },
-    [locationIds.STORE]: {
+    STORE: {
         BUY_MAP: 'buy_map',
         BUY_GUIDE: 'buy_guide',
         BUY_DOLL: 'buy_doll',
@@ -32,7 +30,7 @@ export const specificActions: Record<LocationId, Record<Action, string>> = {
         BUY_BLACK_ROCK: 'buy_black_rock',
         BUY_BUBBLE_PASS: 'buy_bubble_pass',
     },
-    [locationIds.STATUE]: {
+    STATUE: {
         EXPLORE: 'explore',
         DECODE_ENGRAVING: 'decode_engraving',
         CAST_COOLING_AURA: 'cast_cooling_aura',
@@ -40,38 +38,38 @@ export const specificActions: Record<LocationId, Record<Action, string>> = {
         POWER_CONTAINMENT: 'power_containment',
         PURIFY_CORRUPTION: 'purify_corruption',
     },
-    [locationIds.LIBRARY]: {
+    LIBRARY: {
         EXPLORE: 'explore',
         STUDY_CRIMSON: 'study_crimson',
         STUDY_ARTEFACT: 'study_artefact',
         DECODE_ARTEFACT: 'decode_artefact',
     },
-    [locationIds.ANCHOVY]: {
+    ANCHOVY: {
         EXPLORE: 'explore',
         INSPIRE: 'inspire',
     },
-    [locationIds.BARNACLE]: {
+    BARNACLE: {
         EXPLORE: 'explore',
         HELP: 'help',
     },
-    [locationIds.SALMON]: {
+    SALMON: {
         EXPLORE: 'explore',
         CONFRONT: 'confront',
     },
-    [locationIds.KELP]: {
+    KELP: {
         EXPLORE: 'explore',
         CLIMB_DOWN: 'climb_down',
         HARVEST: 'harvest',
     },
-    [locationIds.SHRINE]: {
+    SHRINE: {
         GIVE_HAIR: 'give_hair',
     },
-    [locationIds.UMBRAL]: {
+    UMBRAL: {
         EXPLORE: 'explore',
         GIVE_PAN: 'give_pan',
         GIVE_ROCK: 'give_rock',
     },
-    [locationIds.WOODS]: {
+    WOODS: {
         GET_HAIR: 'get_hair',
     }
 }
@@ -81,9 +79,10 @@ const allUnderseaActions = Object.values(ALL_UNDERWATER);
 
 export const actionsByLocation: Record<LocationId, Action[]> = {};
 Object.entries(specificActions).forEach(([k, v]) => {
-    actionsByLocation[k] = Object.values(v);
-    if (locationsMapping[k].undersea)
-        actionsByLocation[k].push(...allUnderseaActions);
-    if (locationsMapping[k].oxygenStream)
-        actionsByLocation[k].push(...allOxygenActions);
+    const locationId = locationIds[k];
+    actionsByLocation[locationId] = Object.values(v);
+    if (locationsMapping[locationId].undersea)
+        actionsByLocation[locationId].push(...allUnderseaActions);
+    if (locationsMapping[locationId].oxygenStream)
+        actionsByLocation[locationId].push(...allOxygenActions);
 })
