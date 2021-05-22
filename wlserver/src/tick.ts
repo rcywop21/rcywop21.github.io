d8b8c3157003706a39638cbd871c4601d5e8134e
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-23 00:22:04 +0800

    Begin logging player death

--- wlserver/src/tick.ts
+++ wlserver/src/tick.ts
@@ -1,5 +1,6 @@
 import { TeamId } from "wlcommon";
 import { notifyPlayerState } from "./connections";
+import logger from "./logger";
 import { applyTransform, gameState, killTransform } from "./stateMgr";
 
 const onTick = (): void => {
@@ -11,6 +12,7 @@ const onTick = (): void => {
 
         if (playerState.oxygenUntil && playerState.oxygenUntil.valueOf() < currentTime) {
             applyTransform(killTransform, playerId);
+            logger.log('info', `Player ${playerId} ran out of Oxygen.`)
             notifyPlayerState(playerId);
         }
     }
