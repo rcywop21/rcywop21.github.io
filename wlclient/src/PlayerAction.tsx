116d302653d6c0e29bf8e12815b8eeb33d1380da
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-25 13:49:34 +0800

    Fix quests not being shown sometimes

--- wlclient/src/PlayerAction.tsx
+++ wlclient/src/PlayerAction.tsx
@@ -1,16 +1,24 @@
+import { PlayerState } from "wlcommon";
+
+export type StatePredicate = (playerState: PlayerState) => boolean;
+
 export class PlayerAction {
     description: string;
     task: string;
     x: string;
     y: string;
+    getVisibility?: StatePredicate;
+    getEnabled?: StatePredicate;
     isVisible: boolean;
     isEnabled: boolean;
-    constructor(description: string, task: string, x: string, y: string) {
+    constructor(description: string, task: string, x: string, y: string, visibility?: StatePredicate, enabled?: StatePredicate) {
         this.description = description;
         this.task = task;
         this.x = x;
         this.y = y;
         this.isVisible = true;
         this.isEnabled = true;
+        this.getVisibility = visibility;
+        this.getEnabled = enabled;
     }
 }
