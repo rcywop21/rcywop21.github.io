25cc9a0e22c8748940f4d3d1a6478cf1b392e3fc
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-25 22:49:49 +0800

    Add hostname specifier

--- wlserver/src/socket.ts
+++ wlserver/src/socket.ts
@@ -5,6 +5,7 @@ import { Server } from 'socket.io';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
 const ALLOWED_ORIGINS =
     (process.env.ORIGINS && process.env.ORIGINS.split(',')) || '*';
 
@@ -19,6 +20,6 @@ app.use(expressLogger);
 app.get('/', (_, res) => res.send('This is a backend server.'));
 
 export const listen = (): http.Server =>
-    server.listen(PORT, () => {
+    server.listen(PORT, HOSTNAME, () => {
         logger.log('info', `[server]: Server is running on *:${PORT}`);
     });
