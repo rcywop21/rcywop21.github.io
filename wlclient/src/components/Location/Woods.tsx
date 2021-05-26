e941a5711aa73fa42bd54ea7232b6f4611fe25e9
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-26 11:25:55 +0800

    Make it only possible to get one unicorn hair/tear
    Fixes #57

--- wlclient/src/components/Location/Woods.tsx
+++ wlclient/src/components/Location/Woods.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Action, ActionProps } from './Action';
 import { SpecificLocationProps, imgDirectoryGenerator } from './LocationComponent';
-import { Actions } from 'wlcommon';
+import { Actions, itemDetails, itemsById } from 'wlcommon';
 import { makeActionProps, PlayerAction } from '../../PlayerAction';
 
 const actions: Record<string, PlayerAction> = {
     [Actions.specificActions.WOODS.GET_HAIR]: new PlayerAction("Get Hair", "It is said that a herd of unicorns live in the Whispering Woods, and they only appear to the pure of heart. Fortunately, we only need to find the hair that they shed.", 
-        "Share a point in time when you received help and support from others to receive 1 X Unicorn Hair.", "703px", "382px")
+        "Share a point in time when you received help and support from others to receive 1 X Unicorn Hair.", "703px", "382px",
+        undefined,
+        (playerState) => !(playerState.inventory[itemDetails.UNICORN_HAIR.id]?.qty || playerState.inventory[itemDetails.UNICORN_TEAR.id]?.qty)
+                                                              )
 }
 
 const Woods = (props: SpecificLocationProps): React.ReactElement => {
