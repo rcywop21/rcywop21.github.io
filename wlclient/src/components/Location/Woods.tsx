993d7854dfeff9bbe3472fbcccb88a80f0268642
Lhwhatever <llinhong.2301+github@gmail.com>
2021-05-26 17:20:36 +0800

    Make explicit that mentor decides for Get Hair
    Make explicit that mentor decides number of people who need to share for
    the Get Unicorn Hair action.
    
    Fixes #36

--- wlclient/src/components/Location/Woods.tsx
+++ wlclient/src/components/Location/Woods.tsx
@@ -6,7 +6,7 @@ import { makeActionProps, PlayerAction } from '../../PlayerAction';
 
 const actions: Record<string, PlayerAction> = {
     [Actions.specificActions.WOODS.GET_HAIR]: new PlayerAction("Get Hair", "It is said that a herd of unicorns live in the Whispering Woods, and they only appear to the pure of heart. Fortunately, we only need to find the hair that they shed.", 
-        "Share a point in time when you received help and support from others to receive 1 X Unicorn Hair.", "703px", "382px",
+        "Share a point in time when you received help and support from others to receive 1 X Unicorn Hair. Your mentors will decide how many of you needs to share.", "703px", "382px",
         undefined,
         (playerState) => !(playerState.inventory[itemDetails.UNICORN_HAIR.id]?.qty || playerState.inventory[itemDetails.UNICORN_TEAR.id]?.qty)
                                                               )
